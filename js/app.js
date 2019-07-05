97ee55d64057acb47ec8f66b78c4bbdaa24a4fcd
jojcurr <curr0250@algonquinlive.com>
2019-07-05 15:01:14 -0400

    stars
    stars now disappear depending on ranking of number of moves

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //create list of cards
-
 let cards = ['fa-diamond', 'fa-diamond',
             'fa-paper-plane-o', 'fa-paper-plane-o',
              'fa-anchor', 'fa-anchor',
@@ -11,7 +10,6 @@ let cards = ['fa-diamond', 'fa-diamond',
             ];
 
 //create the HTML element for each card
-
 function createCard(card) {
     return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 };
@@ -20,20 +18,15 @@ function createCard(card) {
 let totalSec = 0;
 let timeUsed = setInterval(countTimer, 1000);
 
+let deck = document.querySelector('.deck');
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
+//create cards to play game
 function createGame() {
-    let deck = document.querySelector('.deck');
     let cardHTML = shuffle(cards).map(function (card) {
         return createCard(card);
     });
     deck.innerHTML = cardHTML.join('');
+    startGame();
     countTimer();
 }
 createGame();
@@ -59,20 +52,41 @@ let showMoves = document.querySelector('.moves');
 
 //keeps track of and displays number of moves
 function keepCount() {
-    moves ++;
+    moves++;
     showMoves.textContent = `Moves: ${moves}`;
+    setStars(moves);
 }
 
+let star1 = document.getElementById("star1");
+let star2 = document.getElementById("star2");
+let star3 = document.getElementById("star3");
+
+//controlnumber of stars
+function setStars(moves) {
+
+
+    if (moves == 2) {
+        star1.classList.add('close');
+    }
+
+    if (moves == 4) {
+        star2.classList.add('close');
+    }
+
+    if (moves == 6) {
+        star3.classList.add('close');
+    }
+};
 
 //keep track of and display time
-
 function countTimer() {
-    ++totalSec;
+    totalSec++;
     let hour = Math.floor(totalSec / 3600);
     let min = Math.floor((totalSec - hour * 3600) / 60);
     let sec = totalSec - (hour * 3600 + min * 60);
     document.querySelector(".timer").innerHTML = min + ":" + sec;
 };
+
 
 
 //stop time
@@ -86,21 +100,21 @@ let shownCards = [];
 let matchedPairs = [];
 let win = false;
 
-//add event listener to cards
-deckOfCards.forEach(function (card) {
-    card.addEventListener('click', (function (ev) {
-        if ((!card.classList.contains('show')) && shownCards.length < 2) {
-            shownCards.push(card);
-            showCards(card);
 
+
+function startGame() {
+    deck.addEventListener('click', function (e) {
+        if (e.target && e.target.nodeName == "LI" && (!e.target.classList.contains('show')) && shownCards.length < 2) {
+            shownCards.push(e.target);
+            showCards(e.target);
         }
-
-
         if (shownCards.length % 2 === 0) {
-            hideCards(card);
+            hideCards();
         }
-    }));
-});
+    });
+    console.log("start gamefunction working");
+}
+
 
 //flip selected cards over
 function showCards(card) {
@@ -145,22 +159,28 @@ function checkMatch(card) {
     }
 };
 
+//reset Game
 let reset = document.querySelector(".restart");
-reset.addEventListener("click", function(ev){
+reset.addEventListener("click", resetGame);
+
+function resetGame() {
     totalSec = 0;
     moves = 0;
+    console.log("gotit");
     showMoves.textContent = `Moves: ${moves}`;
     createGame();
-});
-////reset game
-//function resetGame(){
-//    
-//}
+    star1.classList.remove('close');
+    star2.classList.remove('close');
+    star3.classList.remove('close');
+}
+
 
 //modal
 let modal = document.querySelector(".modal");
 let closeButton = document.querySelector(".close-button");
 let fireModal = document.querySelector(".trigger");
+let yes = document.querySelector(".yes");
+//let no = document.querySelector(".no");
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -168,20 +188,16 @@ function toggleModal() {
 
 fireModal.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
+yes.addEventListener("click", function () {
+    resetGame();
+    toggleModal();
+});
 
-
+let finalTime =document.querySelector("finalTime");
 function winGame() {
+    if (win = yes) {
+        //finalTime.textContent = ${timeUsed};
+    }
     fireModal.click();
 
-}
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+};
